53c0d865ab851e145f0ed57591029cf0649a5cf6
Grzegorz Gackowski <greggupl1@gmail.com>
2020-07-05 19:19:29 +0200

    configuration CLI added

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from controller import Controller
 import paho.mqtt.client as mqtt
 import tkinter as tk
 import time
+import sys
 
 rooms = {}
 
@@ -19,15 +20,31 @@ def on_message(client, userdata, msg):
 def run_mqtt():
     client = mqtt.Client()
     client.on_message = on_message
-    client.connect('localhost', 1883, 60)
-    client.subscribe('ok')
+    client.connect(mqtt_host, mqtt_port, 60)
+    client.subscribe(mqtt_topic)
     client.loop_start()
 
 
-def main():
+def main(argv):
+
+    if len(sys.argv) < 5:
+        print("Not enough arguments!")
+        print("python3 main.py <json config file> <mqtt host> <mqtt port> <mqtt topic>")
+        exit()
+
+    global config_filepath
+    global mqtt_host
+    global mqtt_port
+    global mqtt_topic
+    
+    config_filepath = sys.argv[1]
+    mqtt_host = sys.argv[2]
+    mqtt_port = int(sys.argv[3])
+    mqtt_topic = sys.argv[4]
+
     visualizer = Visualizer('Visual Smart House', 640, 480)
     global rooms
-    rooms = load_rooms_data_from_json('/data/default.json', visualizer) 
+    rooms = load_rooms_data_from_json(config_filepath, visualizer) 
 
     controller = Controller(rooms, visualizer)
 
@@ -36,4 +53,4 @@ def main():
     visualizer.main_loop()
 
 if __name__ == '__main__':
-    main()
+    main(sys.argv)
